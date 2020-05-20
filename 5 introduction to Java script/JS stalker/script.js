07cda2ccef2eedaed01454eddf85e236c3e1629a
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-05-21 02:27:10 +0530

    Js stalker assignment added

---
+++ 5 introduction to Java script/JS stalker/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is ur first name");
+var lastName = prompt("What is your last name");
+var age = prompt("What is your age")
+
+console.log("your full name is " + firstName + " " + lastName);
+console.log("you are" + age + "years old!!") 
---
+++ 5 introduction to Java script/script.js
@@ -0,0 +1,5 @@
+var userName = prompt("What is ur name");
+
+alert("Nice to meet you " + userName );
+
+console.log("also nice to meet you, " + userName);
