25ef006bc1548009f27dba16d818be8d13ab7e06
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-05-21 19:18:30 +0530

    7th section of the course statted

---
+++ entry.js
@@ -0,0 +1,17 @@
+var age = Number(prompt("What is your age"));
+if (age<0){
+    console.log("try when you come out of your womb");
+}
+
+else if (age <18){
+    console.log("Sorry you can not enter inside ");
+
+}
+
+else if (age<21){
+    console.log("You can only enter but cannot have drinks ");
+}
+
+else {
+    console.log("you can enjoy the drinks");
+}
