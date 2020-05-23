8b328f5c2130b8fdedb008226159e46a46e19ff3
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-05-24 02:54:07 +0530

    arry js file added

---
+++ 8 java script basice Arrays/intro to array.js
@@ -0,0 +1,13 @@
+var friend1 = "sandeep";
+var friend2 = "sunil"
+var friend3 = "anil";
+var friend4 = "Shivu"
+
+// As we can see that there is a lot of repeated code (Not considered as DRY)
+
+// rather than having 4 saperate variable , we can do it in one variable
+
+var friends = ["sandeep", "sunil", "anil", "shivu"]
+//here "sandeep" takes 0th place 
+
+
