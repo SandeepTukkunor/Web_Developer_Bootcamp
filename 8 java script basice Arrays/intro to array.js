ded1a3394447dd00730df5afd01e36eb89b9025a
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-05-24 03:13:33 +0530

    intro section completed

--- 8 java script basice Arrays/intro to array.js
+++ 8 java script basice Arrays/intro to array.js
@@ -10,4 +10,19 @@ var friend4 = "Shivu"
 var friends = ["sandeep", "sunil", "anil", "shivu"]
 //here "sandeep" takes 0th place 
 
+// for accesing the perticualr variable from the array
+// assume i want to grab sandeep from variable friends , i can just type
+console.log(friends[0]); // this will grab the 0th element of the variable
+
+// we can also chage the elements in the variable
+friends[0] = "Nagu"; // from the past output we have got 0th element is sandeep , and it will be replaced by Nagu
+
+// and we may have noticed that there are 4 element in the array and indexing starts from 0, so basically we have from 0 to 3
+// What if we want to add another friends name
+friends[4] = "Sagar";// the 4th element of the arry 
+
+//once we run this , we will get 
+["Nagu", "sunil", "anil", "shivu","Sagar" ]
+
+
 
