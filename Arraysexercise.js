90c348c109442ac36f4345497b5c234c8db630d7
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-05-24 19:53:42 +0530

    arrays exercse added

---
+++ Arraysexercise.js
@@ -0,0 +1,14 @@
+var numbers = [22,34,54,32,45];
+
+console.log(numbers[numbers.length]);
+
+
+
+// if we take numbers[4] : output will be 45
+
+var friends_group = [["harry", "ron", "albi"],
+                    ["virat", "Rohit", "Rahul"],
+                ["smith", "kevin", "warner"]];
+
+//console.log(friends_group[2][0])
+//we will get smith as output  because it is present in 2nd array of the array and 1st word of 2nd array
