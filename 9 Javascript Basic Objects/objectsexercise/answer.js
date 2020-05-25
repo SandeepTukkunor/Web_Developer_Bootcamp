ff318b1986de2e981e566ef6d0ff5dc5cac44c5b
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-05-25 19:51:56 +0530

    exercise completed

---
+++ 9 Javascript Basic Objects/objectsexercise/answer.js
@@ -0,0 +1,38 @@
+var someObject = {};
+
+someObject.name = "sandeep"; // added the name to object
+
+someObject.age = 21;
+
+
+// now lets try adding a variable prop to someObject 
+
+var prop = "color"
+
+someObject["prop"] = 'red';
+
+
+//someObject.123 = true;
+// the one above wont work because , it starts with number 
+
+
+
+
+
+//exercise 2
+
+var  someObject = {
+    friends: [
+        {name : "sandeep"},
+        {name : "Sunil"},
+        {name : "Anil"}
+
+    ],
+
+    color: "baby blue",
+    isEvil : true
+};
+
+
+
+
