68fb89ea786c2e7243fbc81838a9bf63a9d3e88c
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-05-25 19:10:44 +0530

    intro to objects added

---
+++ 9 Javascript Basic Objects/intro.js
@@ -0,0 +1,34 @@
+// in this session we are discussing objects 
+// they are similar to the dictionaries in the python language
+
+//ex 
+
+var person = {name: "sandeep",
+
+age : 21,
+place : "Ghogga"}
+
+
+// if u want to access the perticular part of the object  
+// console.log(person.name)
+// (or) console.log(person["name"]);
+
+
+
+
+
+
+
+
+// if we want to replace something in the ibject, 
+
+person["age"] += 1;
+person["place"] = "humnabad"
+
+// or it can be replace like
+person.age = 23
+
+//we have 2 options, use dot natation or square bracket notation 
+
+
+
