400cca6efe4eaff2a22e283c9f0bf15fb6bba51d
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-05-26 19:54:04 +0530

    intro  to DOM added

---
+++ 10 Intro to DOM/IMP selector methods/dom.js
@@ -0,0 +1,30 @@
+//var tag = document.getElementById("highlight");
+
+// we will get the line with id highlight 
+
+//var tags = document.getElementsByClassName("bolded");
+
+// the above line will get all the elements by its class name 
+
+
+
+//var tagi = document.getElementsByTagName("li"); 
+
+// the above line of code will get all the elements with the similar tagnames 
+
+
+
+
+
+// and similarly there are other methods with which we can select css part of the document 
+//var li = document.querySelector("#highlight");
+
+// it will return everything with css highlight 
+
+
+
+
+
+
+
+
