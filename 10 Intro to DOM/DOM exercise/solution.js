ea6e3add3534e50a72e0ef66896d1e1b015ccd38
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-05-26 20:44:00 +0530

    exercise completed

---
+++ 10 Intro to DOM/DOM exercise/solution.js
@@ -0,0 +1,16 @@
+//document.getElementById("first");
+//OUTPUT : <p id="first" class="special">Hello</p>
+
+
+//document.querySelector("#first")
+//OUTPUT: <p id="first" class="special">Hello</p>
+
+
+
+//document.getElementsByClassName("special");
+
+// Output:
+// <p id="first" class="special">Hello</p>
+// <p class="special">Good Bye </p>
+
+// if we use based on tags , we will get all the outputs with tag paragraph 
