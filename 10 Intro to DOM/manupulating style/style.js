9e43e1e96eef23cdd1339543ff210ae8f4d2a6d5
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-05-26 23:33:35 +0530

    manupulating style added

---
+++ 10 Intro to DOM/manupulating style/style.js
@@ -0,0 +1,12 @@
+// selecting 
+//var tag = document.querySelector("h1");
+
+
+// manipulate
+//tag.style.color = "green";
+
+// this will change the color to yellow 
+
+
+// tag.style.border = "5px solid pink"
+// adds the border 
