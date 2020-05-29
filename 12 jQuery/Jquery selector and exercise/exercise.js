5c1c2c78020bbde9999c6b71b793c344fd24b9fb
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-05-29 11:59:54 +0530

    exercise completed

---
+++ 12 jQuery/Jquery selector and exercise/exercise.js
@@ -0,0 +1,17 @@
+//purple background
+$("div").css("background", "purple");
+
+
+
+
+//select the div with class highlight
+$(".highlight").css("width", "200px")
+
+
+
+//select the div with id third and give it  orange border
+$("third").css("border", "2px solif orange")
+
+
+//select 1st div and change color to div
+$("div:first-of-type").css('color', 'pink')
