df02641c839a340a33f6820df31b6b88e762e80a
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-05-30 13:14:03 +0530

    effect file

---
+++ 13 Advanced JQuery/JQuery Effects/effects.js
@@ -0,0 +1,20 @@
+// $("button").on("click", function(){
+//     $("div").fadeOut(1000 , function(){
+//        // $(this).remove()
+//     });
+//     console.log("fade completed")
+// });
+
+
+
+
+// Fade in 
+// in css display = none;   
+$("button").on("click", function(){
+    $("div").fadeIn(1000 , function(){
+       // $(this).remove()
+    });
+    console.log("fade completed")
+});
+
+
